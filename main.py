54bedf381da31e15cf99158ea9712c7591ee0f90
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-11-24 21:37:33 +0900

    commit

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ import argparse
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-def remove_background(img):
-    mask = np.zeros(img.shape[:2], np.uint8)
-    bgdModel = np.zeros((1, 65), np.float64)
-    fgdModel = np.zeros((1, 65), np.float64)
-    rect = (10, 10, img.shape[1] - 20, img.shape[0] - 20)  # 이미지 가장자리 제외
-    cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
-    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-    img = img * mask2[:, :, np.newaxis]
-    return img
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -32,11 +23,6 @@ if __name__ == '__main__':
         model_img = img
         print("Already (768, 1024)")
     
-    # 배경 제거 옵션 적용
-    if opt.background:
-        print("Removing background...")
-        model_img = remove_background(model_img)
-
     # 저장
     cv2.imwrite("./model.jpg", model_img)
 
@@ -97,7 +83,7 @@ if __name__ == '__main__':
     print("\nGenerate Human Parsing agnoistic")
     terminnal_command = "python get_agnostic.py --data_path HR-VITON/test/test --output_path HR-VITON/test/test/agnostic-v3.2"
     os.system(terminnal_command)
-    
+
     # Run HR-VITON to generate final image
     print("\nRun HR-VITON to generate final image\n")
     os.chdir("./HR-VITON")
